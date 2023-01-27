6e6fde7ee944e13c68230c1932930cd84eb1ea42
gans92 <gabri.alisson.23@gmail.com>
2023-01-27 10:32:45 -0500

    correções no env

--- src/connection/BaseDatabase.ts
+++ src/connection/BaseDatabase.ts
@@ -11,7 +11,7 @@ export class Database {
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
-        port: 3306
+        port: 3003
      }
     })
 }
