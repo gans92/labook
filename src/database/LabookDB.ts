d03869b43174da6d45662c971d4ed51408055a48
gans92 <gabri.alisson.23@gmail.com>
2023-01-27 12:12:49 -0500

    teste9

--- src/database/LabookDB.ts
+++
@@ -1,4 +0,0 @@
-import { LabookRepository } from "../business/LabookRepository";
-
-import { CustomError } from "../error/CustomError";
-import { makeFriend, post, user, like, commentModel } from "../model/types";
